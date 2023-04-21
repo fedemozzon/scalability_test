e92008d00c98a8fa13636bec8af4e28e0d311eb3
Manuel Martinez <manuel.martinez@ripio.com>
2023-04-20 22:44:00 -0300

    mejoras script neo4j

--- scripts/test_session_neo4j.py
+++ scripts/test_session_neo4j.py
@@ -12,19 +12,42 @@ from random import randint
 from time import perf_counter
 import concurrent.futures
 
+# Constantes
+URL = "bolt://localhost:7687" # Fijarse el puerto de cada nodo mapeado
+DB_NAME = "neo4j2"
+USER = "neo4j"
+PASSWORD = "fedeymanu"
+
 class DatabaseConnection():
-    def __init__(self, uri, user, password) -> None:
-        self.driver = GraphDatabase.driver(uri, auth=(user, password))
+    def __init__(self, uri, user, password, database) -> None:
+        self.driver = GraphDatabase.driver(uri, auth=(user, password), database=database)
 
     def close(self):
         self.driver.close()
 
+    @staticmethod
     def _write(txn, name):
         query = ("CREATE (n:Person {name: $name, id: randomUUID()}) "
                  "RETURN n.id AS node_id")
         result = txn.run(query, name=name)
+        # Result.single() retorna un objeto neo4j.Record
         record = result.single()
+        # Se le puede acceder a los campos del record como si fuera un diccionario
         return record["node_id"]
+
+    @staticmethod
+    def _read(txn):
+        query = ("MATCH (n) RETURN n LIMIT 100")
+        # https://neo4j.com/docs/api/python-driver/current/api.html#result
+        # txn.run retorna objeto neo4j.Result
+        result = txn.run(query)
+        # result.data() retorna los resultados como una lista de diccionarios
+        result_list = result.data()
+        return result_list
+
+    def read(self):
+        with self.driver.session() as session:
+            return session.execute_read(self._read)
 
     def insert(self, name):
         with self.driver.session() as session:
@@ -50,5 +73,11 @@ logging.basicConfig(format=format, level=logging.INFO,
 # with concurrent.futures.ThreadPoolExecutor(max_workers=THREADS_SIZE) as executor:
 #         executor.map(thread_function, range(THREADS_SIZE))
 
-db = DatabaseConnection("bolt://localhost:7687", "neo4j", "fedeymanu")
-db.insert("hola")
+# Conexion a la db
+db = DatabaseConnection(URL, USER, PASSWORD, DB_NAME)
+
+# Insert para crear cosas (podemos cambiar la query)
+# db.insert("hola")
+
+# Por ahora el read retorna match 100 nodos cualquiera
+print(db.read())
