584c10c1e3ccc5cfb333a33036c1227a25041c36
Manuel Martinez <manuel.martinez@ripio.com>
2023-09-11 20:34:00 -0300

    script de grafico para mas archivos

--- scripts/jueguitos/create_graph_results.py
+++ scripts/jueguitos/create_graph_results.py
@@ -2,47 +2,39 @@ import sys
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Cargar nombre del archivo CSV como primer argumento
+# Check for the correct number of command-line arguments
 if len(sys.argv) < 5:
-    print("Uso: python create_graph_results.py <filename> <intervalo filas> <titulo nodo> <db>")
+    print("Uso: python create_graph_results.py <filename1> <filename2> <filename3> <intervalo filas> <titulo nodo> <db>")
     sys.exit(1)
 
-filename = sys.argv[1]
-intervalo = int(sys.argv[2])
-titulo_nodo = sys.argv[3]
-db = sys.argv[4]
+filenames = sys.argv[1:4]
+interval = int(sys.argv[4])
+node_title = sys.argv[5]
+db = sys.argv[6]
 
-# Cargar el archivo CSV
-archivo_csv = filename
-dataframe = pd.read_csv(archivo_csv)
+# Crear figura
+plt.figure(figsize=(12, 6))
 
-# Obtener los valores de la columna "Tiempos Individuales"
-valores_tiempo = dataframe["Tiempo Individual"].tolist()
+# Recorrer archivos
+for i, filename in enumerate(filenames):
+    dataframe = pd.read_csv(filename)
+    
+    # Extraer datos de columna "Tiempo Individual"
+    valores_tiempo = dataframe["Tiempo Individual"].tolist()
+    
+    # Calcular el promedio del intervalo
+    moving_avg = []
+    for j in range(0, len(valores_tiempo) - interval + 1, interval):
+        avg = sum(valores_tiempo[j:j+interval]) / interval
+        moving_avg.append(avg)
 
-# Crear el gráfico de evolución de tiempo con promedio cada 1000 filas
-plt.figure(figsize=(40, 6))  # Tamaño del gráfico (opcional)
+    # Graficar los datos
+    plt.plot(range(0, len(moving_avg) * interval, interval), moving_avg, label=filename.split("_")[1])
 
-# Configurar etiquetas y título del gráfico
-plt.xlabel(f"Número de filas (cada {intervalo})")
+# Configurar titulos y labels
+plt.xlabel(f"Número de filas (cada {interval})")
 plt.ylabel("Tiempo Promedio")
-plt.title(f"{db}: Evolución del Tiempo con Promedio cada {intervalo} filas de {titulo_nodo}")
+plt.title(f"{db}: Evolución del Tiempo con Promedio cada {interval} filas de {node_title}")
+plt.legend()
 
-# Calcular el promedio cada 1000 filas
-promedios = []
-sobrante = len(valores_tiempo) % intervalo
-
-for i in range(0, len(valores_tiempo) - sobrante, intervalo):  # Ignorar las últimas 70 filas
-    promedio = sum(valores_tiempo[i:i+intervalo]) / intervalo
-    promedios.append(promedio)
-
-# Añadir el promedio de las últimas 70 filas
-promedios.append(sum(valores_tiempo[-sobrante:]) / sobrante)
-
-# Crear una lista de índices correspondientes al promedio cada 1000 filas
-indices = [i * intervalo for i in range(len(promedios))]
-
-# Dibujar los datos en el gráfico
-plt.plot(indices, promedios)
-
-# Mostrar el gráfico
 plt.show()
